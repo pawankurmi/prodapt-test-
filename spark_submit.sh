4e9eb873c98db69fab5c9a7883f83cdf8ddee4dc
pawankurmi <82380708+pawankurmi@users.noreply.github.com>
2021-04-19 01:02:58 +0530

    Delete spark_submit.sh

--- spark_submit.sh
+++
@@ -1,9 +0,0 @@
-/usr/hdp/current/spark2-client/bin/spark-submit \
- --class com.prodapt.assignment.StreamAssignment \
- --master yarn \
- --deploy-mode cluster \
- --driver-memory 2g \
- --executor-memory 2g \
- --num-executors 2 \
- --executor-cores 2 \
---queue <queue-name> <jar-file-path> "<input file path>" "<output dir>"
